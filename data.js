0d0eeff258a3d15d2a3ff83d68b73e8d6b7b06ea
pmzou <pmzoupmzou@gmail.com>
2026-09-16 14:45:25 +0900

    Add per-image cache versioning

--- data.js
+++ data.js
@@ -5,8 +5,8 @@
   if(x.status>=200 && x.status<300) (0,eval)(x.responseText);
   else window.COSTUMES=[];
   window.COSTUMES.push(
-    {"id":101,"name":"No.101｜REALIZE 肌色×黒メッシュ水着","tags":["fullback"],"visible":true,"image":"images/101.jpg"},
-    {"id":102,"name":"No.102｜REALIZE 青×白水着","tags":["fullback"],"visible":true,"image":"images/102.jpg"}
+    {"id":101,"name":"No.101｜REALIZE 肌色×黒メッシュ水着","tags":["fullback"],"visible":true,"image":"images/101.jpg","imageVersion":1},
+    {"id":102,"name":"No.102｜REALIZE 青×白水着","tags":["fullback"],"visible":true,"image":"images/102.jpg","imageVersion":2}
   );
   var grid=document.querySelector('.grid');
   if(grid){
@@ -15,7 +15,8 @@
       if(c.id<=100 || !c.image) continue;
       var card=document.createElement('article');
       card.className='card';
-      card.innerHTML='<img class="photo" src="'+c.image+'" alt="'+c.name+'" loading="lazy"><div class="name"></div><div class="tags"></div>';
+      var imageSrc=c.image+(c.imageVersion?'?v='+encodeURIComponent(c.imageVersion):'');
+      card.innerHTML='<img class="photo" src="'+imageSrc+'" alt="'+c.name+'" loading="lazy"><div class="name"></div><div class="tags"></div>';
       grid.appendChild(card);
     }
   }
